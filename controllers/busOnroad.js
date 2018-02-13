a9b639ce81226ae443f716f314f71ed2a27cb683
CrazyBOY <thammawat_bee@hotmail.com>
2018-02-13 13:10:46 +0700

    test heroic

--- controllers/busOnroad.js
+++ controllers/busOnroad.js
@@ -1,37 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const BusOnroad = require('../busOnroad')
+const User = require('../user')
 const fs = require('fs');
-const PDFDocument = require('./pdfkit-table');
 const blobStream = require('blob-stream');
 var newroad = []
 
 router.get('/', function (req, res) {
-    BusOnroad.find({}, function (err, bus) {
+    User.find({}, function (err, data) {
         if (err) throw err;
-        res.json({ 'bus': bus })
+        res.json({ 'user': data })
     })
-})
-
-router.get('/blob', function (req, res) {
-    var doc = new PDFDocument();
-    var stream = doc.pipe(blobStream());
-    const table0 = {
-        headers: ['Word', 'Comment', 'Summary'],
-        rows: [
-            ['Apple', 'Not this one', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra at ligula gravida ultrices. Fusce vitae pulvinar magna.'],
-            ['Tire', 'Smells like funny', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla viverra at ligula gravida ultrices. Fusce vitae pulvinar magna.'],
-        ],
-    };
-
-    doc.table(table0, {
-        prepareHeader: () => doc.font('Helvetica-Bold'),
-        prepareRow: (row, i) => doc.font('Helvetica').fontSize(12),
-    });
-    doc.end();
-    stream.on('finish', function () {
-        var url = stream.toBlobURL();
-    });
 })
 
 module.exports = router
